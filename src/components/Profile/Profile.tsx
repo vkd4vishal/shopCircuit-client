87c1b981b74ae2b0c066d38dfa1070efccf6af13
Vishal Kumar <vishalkd0066@gmail.com>
2021-12-09 11:35:57 +0530

    feat(backend): added sample code to display image

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,21 +1,61 @@
 import { Button } from "@mui/material";
 import { WithStyles, withStyles } from "@mui/styles";
 import axios from "axios";
+import React from "react";
 const styles = {
   root: {},
+  image: {
+    height: "400px",
+    paddingLeft: "5px",
+  },
 };
 interface ProfileProp extends WithStyles<typeof styles> {}
 
-const ProfileView: React.FC<ProfileProp> = ({ classes }) => {
-  const headers= {
-    userid:'61adcc4f842e5b0f414463a8'
-  }
-  const getUserImage = ()=>{
-    axios.get('/getProfile',{headers}).then(res=> console.log(res)).catch(err=> console.log(err))
-  }
-  return <div className={classes.root}>
-    <Button onClick={getUserImage}>Click Me</Button>
-  </div>
+const ProfileView: React.FC<ProfileProp> =  ({ classes }) => {
+  const [fetched, setFetched] = React.useState(false);
+  const [showImage, setShowImage] = React.useState({ source: "" });
+  
+  // const response = await Axios({
+  //   url: "/getProfile",
+  //   method: "GET",
+  //   responseType: "stream",
+  // });
+  // console.log("response", response);
+  //  response.data.pipe()
+  const headers = {
+    itemId: "61b065266822a02a735fe017",
+  };
+
+  const getUserImage = () => {
+    axios
+      .get("/getItemDetails", { headers,responseType: 'arraybuffer' })
+      .then((response:any) => {
+        const base64 = btoa(
+          new Uint8Array(response.data).reduce(
+            (data, byte) => data + String.fromCharCode(byte),
+            '',
+          ),
+        );
+        setShowImage({ source: "data:;base64," + base64 });
+        
+        console.log(response.data)
+        setFetched(true)
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div className={classes.root}>
+      <Button onClick={getUserImage}>Click Me</Button>
+      {/* <a href={`http://localhost:1234/getProfile`}>hello</a> */}
+      {fetched && (
+        <img
+          className={classes.image}
+          src={showImage.source}
+          alt="facebook"
+        />
+      )}
+    </div>
+  );
 };
 
 const Profile = withStyles(styles)(ProfileView);
