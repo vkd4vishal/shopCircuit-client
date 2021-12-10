5dd180e9d086769a3c5eba89bfd4e9c3965ef421
Vishal Kumar <vishalkd0066@gmail.com>
2021-12-10 14:43:09 +0530

    feat(backend): added delete and reload functionality

--- src/components/ItemMaintenance/itemMaintenance.tsx
+++ src/components/ItemMaintenance/itemMaintenance.tsx
@@ -2,13 +2,16 @@ import { WithStyles, withStyles, StyleRules } from "@mui/styles";
 import * as React from "react";
 import { DataGrid, GridColumns } from "@mui/x-data-grid";
 import axios from "axios";
-import { Button } from "@mui/material";
+import { Button, Collapse } from "@mui/material";
 const styles: StyleRules = {
   root: {},
   itemList: {
     // display: "flex",
     // flexDirection: "row-reverse"
     paddingRight: "5%",
+  },
+  deleteButton: {
+    float: "right",
   },
 };
 const columns: GridColumns = [
@@ -33,22 +36,22 @@ const columns: GridColumns = [
     headerAlign: "center",
     align: "center",
   },
-//   {
-//     field: "price",
-//     headerName: "Price",
-//     type: "number",
-//     flex: 0.5,
-//     headerAlign: "center",
-//     align: "center",
-//   },
-//   {
-//     field: "weight",
-//     headerName: "Weight",
-//     type: "number",
-//     flex: 0.5,
-//     headerAlign: "center",
-//     align: "center",
-//   },
+  //   {
+  //     field: "price",
+  //     headerName: "Price",
+  //     type: "number",
+  //     flex: 0.5,
+  //     headerAlign: "center",
+  //     align: "center",
+  //   },
+  //   {
+  //     field: "weight",
+  //     headerName: "Weight",
+  //     type: "number",
+  //     flex: 0.5,
+  //     headerAlign: "center",
+  //     align: "center",
+  //   },
 ];
 
 interface ItemMaintenanceProp extends WithStyles<typeof styles> {}
@@ -56,38 +59,53 @@ const ItemMaintenanceFormView: React.FC<ItemMaintenanceProp> = ({
   classes,
 }) => {
   const [items, setItems] = React.useState([]);
-  const getData = async () => {
-    await axios
-      .get("/getItems?sellerId=619f2c776c46367d61e27e0e")
+  const [deleteButton, setDeleteButton] = React.useState(0);
+  const [dataReloaded, setDataReloaded] = React.useState(true);
+  const getData =  () => {
+      axios
+      .get("/getItems?sellerId=61b064ae6822a02a735fe011")
       .then((res) => {
         const result = res.data.data.data.docs;
         const itemsList = result.map((item: any) => {
           return { id: item._id, ...item };
         });
         setItems(itemsList);
+        setDataReloaded(false);
+        setDataReloaded(true);
+        setDeleteButton(0)
       });
   };
-
+   
   return (
     <div style={{ height: "90%", width: "100%" }}>
       <Button onClick={getData}>Reload Items</Button>
-      <DataGrid
-        sx={{
-          m: 2,
-          "& .MuiDataGrid-selectedRowCount": {
-            // backgroundColor: 'rgba(255, 7, 0, 0.55)',
-            marginBottom: "35px",
-          },
-        }}
-        rows={items}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-        className={classes.itemList}
-        hideFooterPagination={true}
-        paginationMode={"server"}
-      />
+      <Collapse in={deleteButton === 0 ? false : true}>
+        <Button className={classes.deleteButton}>DELETE</Button>
+      </Collapse>
+      {dataReloaded ? (
+        <DataGrid
+          sx={{
+            m: 2,
+            "& .MuiDataGrid-selectedRowCount": {
+              // backgroundColor: 'rgba(255, 7, 0, 0.55)',
+              marginBottom: "35px",
+            },
+          }}
+          rows={items}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+          className={classes.itemList}
+          hideFooterPagination={true}
+          paginationMode={"server"}
+          onSelectionModelChange={(ids: any) => {
+            setDeleteButton(ids.length);
+          }}
+        />
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
